e73da98393771f236eeb944c53f856e9cd130375
K-Sakanoshita <saka@netfort.gr.jp>
2026-09-19 19:25:51 +0900

    add Event

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,5 +14,11 @@ export default defineConfig({
   },
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 500,
+      },
+    },
   },
 });
